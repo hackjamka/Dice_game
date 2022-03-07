8ea8e3f900b786697ea88e6a4f1ecd3a8603011f
jamka <mentrible99@gmail.com>
2022-03-07 04:07:52 -0500

    Энэ удаа би тоглоом анх эхлэх байдлыг html дээр хийж өмнө хийсэн алдаагаа засаж сайжруулалаа одоо  хэн хожсоныг харуулах үлдлээ

--- app.js
+++ app.js
@@ -21,13 +21,6 @@ var score_1 = document.getElementById("score-1");
 
 var current_All = document.querySelector(".player-current-score");
 
-// id="current-0" нэгдүгээр тоглогчийн одоогийн оноо
-
-var current_0 = document.getElementById("current-0");
-
-// id="current-1" хоёрдугаар тоглогчийн одоогийн оноо
-
-var current_1 = document.getElementById("current-1");
 // src="dice-5.png" alt="Dice" class="dice" шооны зургыг харуулах
 
 var dice_Iamge = document.querySelector(".dice");
@@ -37,39 +30,63 @@ var btn_roll = document.querySelector(".btn-roll");
 
 //class="btn-hold" оноог шилжүүлэх button
 var btn_hold = document.querySelector(".btn-hold");
+//Тоглоомыг шинээр эхлүүлэх
+var btn_new = document.querySelector(".btn-new");
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Анхны төрөх буюу бүх зүйл 0 байна
-score_0.innerHTML=0;
-score_1.innerHTML=0;
-current_All.innerHTML=0;
-dice_Iamge.style.display = "none";
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 //Шоо хаяхад буух зураг ба round score ийг харуулан
 btn_roll.addEventListener("click", function()
 {
+  //Таамгаар тоо сонгох
   var dice_Number = Math.floor(Math.random()*6)+1;
   dice_Iamge.style.display = "block";
   dice_Iamge.src="dice-"+dice_Number+".png";
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //Одоо би тоглогчийн байрыг сольж оноог олгох болно
-  if(activePLayer==0)
-  {
-  if(dice_Number==1){roundScore =0; current_0.innerHTML =roundScore; activePLayer=1;}
-  else{roundScore+=dice_Number; current_0.innerHTML=roundScore;}
+  //Шооны буусан нүд 1 ээс өөр тоо байвал одоогийн оноо нэмнэ
+  if(dice_Number!==1){
+    //Оноог нэмэх
+    roundScore+=dice_Number;
+    //Оноог тоглогчийн одоогийн оноо луу нэмнэ
+    document.getElementById("current-"+activePLayer).innerHTML=roundScore;
+  }else{
+    //Оноог 0 болгон
+    roundScore=0;
+    //Одоогын оноог 0 болгох
+    document.getElementById("current-"+activePLayer).innerHTML=roundScore;
+    // Тоглогчийн ээлжийг солих
+    activePLayer===0 ? activePLayer=1 : activePLayer=0;
+    //Тоглогчид ээлжийн сольсоныг харуулах
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
   }
-  else
-  {
-    if(dice_Number==1){roundScore =0; current_1.innerHTML =roundScore; activePLayer=0;}
-    else{roundScore+=dice_Number; current_1.innerHTML=roundScore;}
-  }
-  // Оноог нийлүүлэх
-  btn_hold.addEventListener("click", function(){
-    if(activePLayer==0){playerScore[0]+=roundScore; score_0.innerHTML= playerScore[0]; current_0.innerHTML=0; activePLayer=1; roundScore=0;}
-    else{playerScore[1]+=roundScore; score_1.innerHTML = playerScore[1]; current_1.innerHTML=0; activePLayer=0; roundScore=0;}
-  })
 })
-if(playerScore[0]>=100) alert("Нэгдүгээр тоглогч хожлоо");
-if(playerScore[1]>=100) alert("Хоёрдугаар тоглогч хожлоо");
+// lass="btn-hold" Оноог нийт оноо луу хийх
+btn_hold.addEventListener("click",function(){
+  // Оноог хийх
+  playerScore[activePLayer]+=roundScore;
+  document.getElementById("score-"+activePLayer).innerHTML=playerScore[activePLayer];
+  //Одоогын оноог 0 болгох
+  roundScore=0;
+  document.getElementById("current-"+activePLayer).innerHTML=roundScore;
+  // Тоглогчийн ээлжийг солих
+  activePLayer===1 ? activePLayer=0 : activePLayer=1;
+  //Тоглогчид ээлжийн сольсоныг харуулах
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+
+})
+// Шинээр эхлүүлэх
+btn_new.addEventListener("click",function(){
+  current_All.innerHTML=0;
+  dice_Iamge.style.display = "none";
+  playerScore = [0,0];
+  activePLayer = 0;
+  roundScore = 0;
+  document.querySelector(".player-0-panel").classList.add("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  score_0.innerHTML=0;
+  score_1.innerHTML=0;
+})
 
 
 
